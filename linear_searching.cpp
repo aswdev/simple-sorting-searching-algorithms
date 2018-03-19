c2132871da4717e1e0c4f65e4a9d9b9ae90e8473
Adam Świątkowski <32803598+aswdev@users.noreply.github.com>
2018-03-19 20:10:20 +0100

    Used C++ formatter to make code easy readable

--- linear_searching.cpp
+++ linear_searching.cpp
@@ -1,46 +1,40 @@
 #include <iostream>
 
 using namespace std;
-void fillthearray(int *arr, int s);
-int linearsearch(int *arr, int s);
-int main()
-{
+
+void fillthearray(int * arr, int s);
+int linearsearch(int * arr, int s);
+int main() {
     int s;
     cout << "Hello! Let's make an array" << endl;
     cout << "What's the size of the array?" << endl;
     cin >> s;
-    while(s<=0)
-    {
+    while (s <= 0) {
         cout << "Array size has to be greater than 0! Try again: ";
         cin >> s;
     }
-    int *arr = new int [s];
-    fillthearray(arr,s);
-    linearsearch(arr,s);
-    delete [] arr;
+    int * arr = new int[s];
+    fillthearray(arr, s);
+    linearsearch(arr, s);
+    delete[] arr;
     return 0;
 }
-void fillthearray(int *arr, int s)
-{
-    for(int i=0; i<s; i++)
-    {
-        cout<<"T["<<i<<"]=";
+void fillthearray(int * arr, int s) {
+    for (int i = 0; i < s; i++) {
+        cout << "T[" << i << "]=";
         cin >> arr[i];
     }
 }
-int linearsearch(int *arr, int s)
-{
-        int number;
-        cout<<"What are you looking for? Enter the number:";
-        cin>>number;
-        for(int i=0; i<s; i++)
-        {
-            if(arr[i]==number)
-            {
-                cout<<"We found your number on position "<<i<<endl;
-                return 0;
-            }
+int linearsearch(int * arr, int s) {
+    int number;
+    cout << "What are you looking for? Enter the number:";
+    cin >> number;
+    for (int i = 0; i < s; i++) {
+        if (arr[i] == number) {
+            cout << "We found your number on position " << i << endl;
+            return 0;
         }
-        cout<<"The number you are looking for does not exsist"<<endl;
-        return 1;
+    }
+    cout << "The number you are looking for does not exsist" << endl;
+    return 1;
 }
